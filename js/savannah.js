8047a4fdfd542a5525f4037af91e25a639d9a1ab
scash15-hub <527SCash@frhsd.com>
2026-05-13 09:31:28 -0400

    started js (kidna confused) following drews

---
+++ js/savannah.js
@@ -0,0 +1,262 @@
+const savannah = [
+    {
+        number: "#1",
+        name: "Bill LeRoy",
+        position: "C",
+        photo: "LeRoy.jpeg",
+        funFact: "Excellent Breakfast Chef",
+        talent: "Trick throw-downs from the catcher position",
+    },
+    {
+        number: "#2",
+        name: "Reese Alexiades",
+        position: "OF",
+        photo: "Alexiades.jpeg",
+        funFact: "Excellent Breakfast Chef",
+        talent: "Trick throw-downs from the catcher position",
+    },
+    {
+        number: "#3",
+        name: "Bryce Grizzaffi",
+        position: "C",
+        photo: "Grizzaffi.jpeg",
+        funFact: "Excellent Breakfast Chef",
+        talent: "Trick throw-downs from the catcher position",
+    },
+    {
+        number: "#4",
+        name: "KJ Jackson",
+        position: "UTL",
+        photo: "KJ.jpeg",
+        funFact: "Excellent Breakfast Chef",
+        talent: "Trick throw-downs from the catcher position",
+    },
+    {
+        number: "#5",
+        name: "DR Meadows",
+        position: "OF",
+        photo: "Meadows.jpeg",
+        funFact: "Excellent Breakfast Chef",
+        talent: "Trick throw-downs from the catcher position",
+    },
+    {
+        number: "#6",
+        name: "Ryan Cox",
+        position: "SS",
+        photo: "Cox.jpeg",
+        funFact: "Excellent Breakfast Chef",
+        talent: "Trick throw-downs from the catcher position",
+    },
+    {
+        number: "#7",
+        name: "Ty Jackson",
+        position: "OF",
+        photo: "Ty.jpeg",
+        funFact: "Excellent Breakfast Chef",
+        talent: "Trick throw-downs from the catcher position",
+    },
+    {
+        number: "#8",
+        name: "Jackson Olson",
+        position: "INF",
+        photo: "Olson.jpeg",
+        funFact: "Excellent Breakfast Chef",
+        talent: "Trick throw-downs from the catcher position",
+    },
+    {
+        number: "#9",
+        name: "Max Jung-Goldberg",
+        position: "UTL",
+        photo: "Jung-Goldberg.jpeg",
+        funFact: "Excellent Breakfast Chef",
+        talent: "Trick throw-downs from the catcher position",
+    },
+    {
+        number: "#10",
+        name: "Alex Ziegler",
+        position: "1B",
+        photo: "Ziegler.jpeg",
+        funFact: "Excellent Breakfast Chef",
+        talent: "Trick throw-downs from the catcher position",
+    },
+    {
+        number: "#11",
+        name: "Austin Drury",
+        position: "P",
+        photo: "Drury.jpeg",
+        funFact: "Excellent Breakfast Chef",
+        talent: "Trick throw-downs from the catcher position",
+    },
+    {
+        number: "#12",
+        name: "Kyle Luigs",
+        position: "P",
+        photo: "Luigs.jpeg",
+        funFact: "Excellent Breakfast Chef",
+        talent: "Trick throw-downs from the catcher position",
+    },
+    {
+        number: "#13",
+        name: "Bryce Stober",
+        position: "Coach",
+        photo: "Stober.jpeg",
+        funFact: "Excellent Breakfast Chef",
+        talent: "Trick throw-downs from the catcher position",
+    },
+    {
+        number: "#14",
+        name: "Dakota Albritton",
+        position: "UTL",
+        photo: "Albritton.jpeg",
+        funFact: "Enjoys spaghetti with ranch",
+        talent: "Trick throw-downs from the catcher position",
+    },
+    {
+        number: "#15",
+        name: "RobertAnthony Cruz",
+        position: "OF",
+        photo: "Cruz.jpeg",
+        funFact: "Was homeschooled most of his life.",
+        talent: "Trick throw-downs from the catcher position",
+    },
+    {
+        number: "#16",
+        name: "Adam Virant",
+        position: "Coach",
+        photo: "LeRoy.jpeg",
+        funFact: "Excellent Breakfast Chef",
+        talent: "Trick throw-downs from the catcher position",
+    }
+        {
+        number: "#17",
+        name: "Easton Parrish",
+        position: "C",
+
+        photo: "LeRoy.jpeg",
+        funFact: "Excellent Breakfast Chef",
+        talent: "Trick throw-downs from the catcher position",
+    }, {
+        number: "#18",
+        name: "Danny Hosley",
+        position: "UTL",
+
+        photo: "LeRoy.jpeg",
+        funFact: "Certified EMT",
+        talent: "Trick throw-downs from the catcher position",
+    }, {
+        number: "#19",
+        name: "Dan Oberst",
+        position: "DH",
+
+        photo: "LeRoy.jpeg",
+        funFact: "Excellent Breakfast Chef",
+        talent: "Trick throw-downs from the catcher position",
+    }, {
+        number: "#20",
+        name: "Grant Besser",
+        position: "P",
+
+        photo: "LeRoy.jpeg",
+        funFact: "Excellent Breakfast Chef",
+        talent: "Trick throw-downs from the catcher position",
+    }, {
+        number: "#21",
+        name: "Jacob Bazala",
+        position: "P",
+
+        photo: "LeRoy.jpeg",
+        funFact: "Excellent Breakfast Chef",
+        talent: "Trick throw-downs from the catcher position",
+    }, {
+        number: "#22",
+        name: "JT Sokolove",
+        position: "DR",
+
+        photo: "LeRoy.jpeg",
+        funFact: "Excellent Breakfast Chef",
+        talent: "Trick throw-downs from the catcher position",
+    }, {
+        number: "#23",
+        name: "Reginald Horton",
+        position: "Inspiration Coach",
+
+        photo: "LeRoy.jpeg",
+        funFact: "Excellent Breakfast Chef",
+        talent: "Trick throw-downs from the catcher position",
+    }, {
+        number: "#24",
+        name: "Derek Klena",
+        position: "RHP",
+
+        photo: "LeRoy.jpeg",
+        funFact: "Excellent Breakfast Chef",
+        talent: "Trick throw-downs from the catcher position",
+    }, {
+        number: "#30",
+        name: "Connor Harris",
+        position: "P",
+
+        photo: "LeRoy.jpeg",
+        funFact: "Excellent Breakfast Chef",
+        talent: "Trick throw-downs from the catcher position",
+    }, {
+        number: "#31",
+        name: "Trystan Levesque",
+        position: "P",
+
+        photo: "LeRoy.jpeg",
+        funFact: "Excellent Breakfast Chef",
+        talent: "Trick throw-downs from the catcher position",
+    }, {
+        number: "#33",
+        name: "Kelsie Whitmore",
+        position: "P",
+
+        photo: "LeRoy.jpeg",
+        funFact: "Excellent Breakfast Chef",
+        talent: "Trick throw-downs from the catcher position",
+    }, {
+        number: "#44",
+        name: "Allan Saathoff",
+        position: "P",
+
+        photo: "LeRoy.jpeg",
+        funFact: "Excellent Breakfast Chef",
+        talent: "Trick throw-downs from the catcher position",
+    }, {
+        number: "#49",
+        name: "Ryan Kellogg",
+        position: "P Coach",
+
+        photo: "LeRoy.jpeg",
+        funFact: "Excellent Breakfast Chef",
+        talent: "Trick throw-downs from the catcher position",
+    }, {
+        number: "#66",
+        name: "Tyler Gillum",
+        position: "Head Coach",
+
+        photo: "LeRoy.jpeg",
+        funFact: "Excellent Breakfast Chef",
+        talent: "Trick throw-downs from the catcher position",
+    }, {
+        number: "#88",
+        name: "Noah Niznik",
+        position: "P",
+
+        photo: "LeRoy.jpeg",
+        funFact: "Loves Mariah Carey",
+        talent: "Trick throw-downs from the catcher position",
+    }, {
+        number: "#99",
+        name: "Frankie Moscatiello",
+        position: "P",
+
+        photo: "LeRoy.jpeg",
+        funFact: "Excellent Breakfast Chef",
+        talent: "Trick throw-downs from the catcher position",
+    },
+];
+
+
+
